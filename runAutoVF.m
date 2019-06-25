3989ba444c315713fad951342f4771777612a937
Petr Kadlec <kadlecp@feec.vutbr.cz>
2019-06-25 11:15:59 +0200

    VFTool v0.1 completed

--- runAutoVF.m
+++ runAutoVF.m
@@ -1,9 +1,20 @@
 function runAutoVF(hObject, eventData)
+%% runAutoVF pushbutton callback to run automatic VF
+% This function collects necessary data and calls the function for automatic VF.
+%
+%  INPUTS
+%   hObject: pushbutton [1 x 1]
+%   eventData: struct, not used
+%
+%  SYNTAX
+%   runAutoVF(hObject)
+%
+% © 2019, Petr Kadlec, BUT, kadlecp@feec.vutbr.cz
 
 fig = hObject.Parent.Parent.Parent;
-
 h = getappdata(fig, 'handles');
 
+%% load settings
 settings.maxTrials = str2num(h.autoEditMaxT.String); %#ok<*ST2NM>
 settings.nIters = str2num(h.autoEditNIters.String);
 settings.rmse = str2num(h.autoEditRmse.String);
@@ -17,17 +28,18 @@ settings.poleDistribution = h.autoPopupDistr.String{h.autoPopupDistr.Value};
 settings.isPeaks = strcmp('complex', ...
    h.autoPopupPoleType.String{h.autoPopupPoleType.Value});
 
+% take data of the reference function
 data = getappdata(fig, 'values');
-if ~isempty(data.funRef)
-   s = 1j*2*pi*data.frequency;
-   [model, funVF, rmse] = automaticVectorFitting(s, data.funRef, settings);
-   data.funVF = funVF;
-   data.model = model;
-   data.rmse = rmse;
-   setappdata(fig, 'values', data)
-   
-   updatePlot(fig)
+if ~isempty (data)
+   if ~isempty(data.funRef)
+      s = 1j*2*pi*data.frequency;
+      [model, funVF, rmse] = automaticVectorFitting(s, data.funRef, settings);
+      data.funVF = funVF;
+      data.model = model;
+      data.rmse = rmse;
+      setappdata(fig, 'values', data)
+      
+      updatePlot(fig)
+   end
 end
-
 end
-
